3e4866bd6ea2c2db37fd7dfe561228f3898f8503
kptoerg-dev <kptoerg@gmail.com>
2026-09-16 23:25:56 +0200

    Create config_template.py

---
+++ config_template.py
@@ -0,0 +1,15 @@
+# config_template.py
+# BITTE UMBENNEN IN config.py und echte Keys eintragen (wird durch .gitignore ignoriert)
+
+EXCHANGE_ID = 'kraken' # oder 'binance'
+API_KEY = 'DEIN_API_KEY_HIER'
+API_SECRET = 'DEIN_API_SECRET_HIER'
+
+# Trading Parameter
+SYMBOL = 'BTC/USDT'
+TIMEFRAME = '15m'
+TRADE_SIZE_EUR = 5.0  # Max 5€ pro Position
+
+# Telegram Benachrichtigungen (für später vorbereitet)
+TELEGRAM_BOT_TOKEN = 'DEIN_TELEGRAM_TOKEN_HIER'
+TELEGRAM_CHAT_ID = 'DEINE_CHAT_ID_HIER'
